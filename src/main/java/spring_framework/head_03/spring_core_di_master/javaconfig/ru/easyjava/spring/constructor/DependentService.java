1abd300fbe5bf5d324d002d3678a9a6f3a82b34a
dimaSkalora <timon2@ukr.net>
2017-10-26 18:44:15 +0300

    03. Внедрение зависимостей. Внедрение с помощью конструктора. JavaConfig

--- src/main/java/spring_framework/head_03/spring_core_di_master/javaconfig/ru/easyjava/spring/constructor/DependentService.java
+++ src/main/java/spring_framework/head_03/spring_core_di_master/javaconfig/ru/easyjava/spring/constructor/DependentService.java
@@ -1,15 +1,9 @@
 package spring_framework.head_03.spring_core_di_master.javaconfig.ru.easyjava.spring.constructor;
 
-import org.springframework.stereotype.Service;
-
-import javax.inject.Inject;
-
-@Service
 public class DependentService {
     private final BaseRepository repository;
     private final BaseService service;
 
-    @Inject
     public DependentService(BaseRepository repository, BaseService service) {
         this.repository = repository;
         this.service = service;
