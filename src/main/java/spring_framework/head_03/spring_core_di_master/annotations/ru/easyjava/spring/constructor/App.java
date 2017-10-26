ee0041fa5313a81a123bbc69274e4c07a652e4f6
dimaSkalora <timon2@ukr.net>
2017-10-26 18:27:13 +0300

    03. Внедрение зависимостей. Внедрение с помощью конструктора. Annotations

---
+++ src/main/java/spring_framework/head_03/spring_core_di_master/annotations/ru/easyjava/spring/constructor/App.java
@@ -0,0 +1,26 @@
+package spring_framework.head_03.spring_core_di_master.annotations.ru.easyjava.spring.constructor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Application main class.
+ */
+public final class App {
+    /**
+     * Do not construct me.
+     */
+    private App() { };
+
+    /**
+     * Application entry point.
+     * @param args Array of command line arguments.
+     */
+    public static void main(final String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("spring_framework.head_03.spring_core_di_master.annotations.ru.easyjava.spring.constructor");
+        DependentService dependent = context.getBean(DependentService.class);
+
+        System.out.println(dependent.process());
+    }
+}
