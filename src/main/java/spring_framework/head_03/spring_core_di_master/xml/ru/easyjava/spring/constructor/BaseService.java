d26b27f413558643430321cc4f80bfab7615ab78
dimaSkalora <timon2@ukr.net>
2017-10-26 19:17:02 +0300

    03. Внедрение зависимостей. Внедрение с помощью конструктора. XML

---
+++ src/main/java/spring_framework/head_03/spring_core_di_master/xml/ru/easyjava/spring/constructor/BaseService.java
@@ -0,0 +1,7 @@
+package spring_framework.head_03.spring_core_di_master.xml.ru.easyjava.spring.constructor;
+
+public class BaseService {
+    Boolean isMany(Integer number){
+        return number > 5;
+    }
+}
---
+++ src/main/java/spring_framework/head_03/spring_core_di_master/xml/ru/easyjava/spring/constructor/DependentService.java
@@ -0,0 +1,18 @@
+package spring_framework.head_03.spring_core_di_master.xml.ru.easyjava.spring.constructor;
+
+public class DependentService {
+    private final BaseRepository repository;
+    private final BaseService service;
+
+    public DependentService(BaseRepository repository, BaseService service) {
+        this.repository = repository;
+        this.service = service;
+    }
+
+    public String process(){
+        if(service.isMany(repository.count()))
+            return "Too big number";
+        else
+            return "Everything is fine";
+    }
+}
