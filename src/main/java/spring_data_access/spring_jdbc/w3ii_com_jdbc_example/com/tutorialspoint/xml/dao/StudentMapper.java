8aa097a2d63493dfa22febbbec5e8e15175931fd
dimaSkalora <timon2@ukr.net>
2017-11-07 19:23:47 +0200

    w3ii.com jdbc example. XML

---
+++ src/main/java/spring_data_access/spring_jdbc/w3ii_com_jdbc_example/com/tutorialspoint/xml/dao/StudentMapper.java
@@ -0,0 +1,17 @@
+package spring_data_access.spring_jdbc.w3ii_com_jdbc_example.com.tutorialspoint.xml.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import spring_data_access.spring_jdbc.w3ii_com_jdbc_example.com.tutorialspoint.xml.model.Student;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentMapper implements RowMapper<Student> {
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Student student = new Student();
+        student.setId(rs.getInt("id"));
+        student.setName(rs.getString("name"));
+        student.setAge(rs.getInt("age"));
+        return student;
+    }
+}
